6ea4504c387dcb997cbc52742972904e43a77a01
solarlime <solarlime@outlook.com>
2019-12-15 17:45:17 +0300

    Task done

---
+++ src/js/game.js
@@ -0,0 +1,19 @@
+import Character from './domain';
+
+class Game {
+  start() {
+    console.log('game started');
+  }
+}
+
+export class GameSavingData {
+}
+
+function readGameSaving() {
+}
+
+function writeGameSaving() {
+}
+
+export default Game;
+export { readGameSaving, writeGameSaving };
